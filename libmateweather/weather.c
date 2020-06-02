00a0b332f1cb8f7ab0b93eea87ecd4a29180ca9f
raveit65 <mate@raveit.de>
2020-06-02 19:53:23 +0200

    Revert "avoid deprecated soup_session_async_new"
    This reverts commit 4cad2cfe818be68e2eb3ef038ab6177cf2fa0bc4.
    
    fixes https://github.com/mate-desktop/libmateweather/issues/75

--- libmateweather/weather.c
+++ libmateweather/weather.c
@@ -553,7 +553,7 @@ _weather_info_fill (WeatherInfo *info,
     info->cb_data = data;
 
     if (!info->session) {
-	info->session = soup_session_new ();
+	info->session = soup_session_async_new ();
 	soup_session_add_feature_by_type (info->session, SOUP_TYPE_PROXY_RESOLVER_DEFAULT);
     }
 
