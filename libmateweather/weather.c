23139abb17f700387d6ad432c816b802b59eb1b8
raveit65 <mate@raveit.de>
2021-07-02 00:15:48 +0200

    use TLS certificate verification for libsoup session

--- libmateweather/weather.c
+++ libmateweather/weather.c
@@ -553,8 +553,9 @@ _weather_info_fill (WeatherInfo *info,
     info->cb_data = data;
 
     if (!info->session) {
-	info->session = soup_session_async_new ();
-	soup_session_add_feature_by_type (info->session, SOUP_TYPE_PROXY_RESOLVER_DEFAULT);
+        info->session = soup_session_async_new ();
+        soup_session_add_feature_by_type (info->session, SOUP_TYPE_PROXY_RESOLVER_DEFAULT);
+        g_object_set (G_OBJECT (info->session), "ssl-use-system-ca-file", TRUE, NULL);
     }
 
     metar_start_open (info);
