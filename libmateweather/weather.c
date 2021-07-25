adfcf56e8a794ef2cefa89f5115067e667c07185
zhuyaliang <15132211195@163.com>
2021-07-25 21:27:11 +0800

    Using soup_session_new replaces the deprecated soup_session_async_new

--- libmateweather/weather.c
+++ libmateweather/weather.c
@@ -553,9 +553,7 @@ _weather_info_fill (WeatherInfo *info,
     info->cb_data = data;
 
     if (!info->session) {
-        info->session = soup_session_async_new ();
-        soup_session_add_feature_by_type (info->session, SOUP_TYPE_PROXY_RESOLVER_DEFAULT);
-        g_object_set (G_OBJECT (info->session), "ssl-use-system-ca-file", TRUE, NULL);
+        info->session = soup_session_new ();
     }
 
     metar_start_open (info);
